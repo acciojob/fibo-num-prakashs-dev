033c3f9accf7a24c55d0b9ad940f162f97ab655c
Prakash S <79446554+prakashs-dev@users.noreply.github.com>
2023-11-12 12:55:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function isFibonacci(num) {
 		// Write your code here
+	let a = 0, b = 1;
+    for (let i = 0; i < num; i++) {
+        let c = a + b;
+        if(num == c) return true;
+        a = b;
+        b = c;
+    }
+  return false;
 }
 const input = parseInt(prompt("Enter a number"));
 alert(isFibonacci(input));
